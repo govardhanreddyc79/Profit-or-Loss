026affbe1579b7560ecd9a21fe3d18e4eed317ac
govardhanreddyc79 <govardhanreddyc79@gmail.com>
2021-09-21 13:03:41 +0530

    added alert message for empty input

--- main.js
+++ main.js
@@ -7,15 +7,12 @@ function submitHandler()
     var initialPrice = inputValues[0].value
     var stockQuantity = inputValues[1].value
     var currentPrice = inputValues[2].value
-    console.log(initialPrice)
-    console.log(stockQuantity)
-    console.log(currentPrice)
     if(initialPrice != '' & stockQuantity != '' & currentPrice != ''){
         if(initialPrice >=0 & stockQuantity >0 & currentPrice >=0){
             calculateProfitAndLoss(initialPrice,stockQuantity,currentPrice)
         }
         else{
-            showMessage("Input values should be greater than 0")
+            showMessage("The input values should be positive")
         }
     }else
     {
@@ -28,7 +25,7 @@ function calculateProfitAndLoss(ip,qty,cp){
     if(cp>ip){
         var profit = (cp - ip)*qty
         var profitPercentage = ((profit/ip)*100).toFixed(2)
-        showMessage("Hey, the profit is "+ profit +" and the percent is " + profitPercentage + "%")
+        showMessage("Awesome!!!, the profit is "+ profit +" and the percent is " + profitPercentage + "%")
     }
     else if(cp===ip){
         showMessage("Hey, no profit and no loss")
@@ -36,7 +33,7 @@ function calculateProfitAndLoss(ip,qty,cp){
     else{
         var loss = (ip - cp)*qty
         var lossPercentage = ((loss/ip)*100).toFixed(2)
-        showMessage("Hey, the loss is "+ loss +" and the percent is " + lossPercentage + "%")
+        showMessage("Sorry, the loss is "+ loss +" and the percent is " + lossPercentage + "%")
     }
 
 }
