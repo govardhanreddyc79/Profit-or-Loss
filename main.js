821fb0de4b2b574a442c1b0b3fe7420bbc23bd04
govardhanreddyc79 <govardhanreddyc79@gmail.com>
2021-09-21 11:58:13 +0530

    fixed code issue

--- main.js
+++ main.js
@@ -7,9 +7,16 @@ function submitHandler()
     var initialPrice = inputValues[0].value
     var stockQuantity = inputValues[1].value
     var currentPrice = inputValues[2].value
-    if(initialPrice & stockQuantity & currentPrice)
-    {
-        calculateProfitAndLoss(initialPrice,stockQuantity,currentPrice)
+    console.log(initialPrice)
+    console.log(stockQuantity)
+    console.log(currentPrice)
+    if(initialPrice != '' & stockQuantity != '' & currentPrice != ''){
+        if(initialPrice >=0 & stockQuantity >0 & currentPrice >=0){
+            calculateProfitAndLoss(initialPrice,stockQuantity,currentPrice)
+        }
+        else{
+            showMessage("Input values should be greater than 0")
+        }
     }else
     {
         alert("Please fill out all Fields")
@@ -22,7 +29,11 @@ function calculateProfitAndLoss(ip,qty,cp){
         var profit = (cp - ip)*qty
         var profitPercentage = ((profit/ip)*100).toFixed(2)
         showMessage("Hey, the profit is "+ profit +" and the percent is " + profitPercentage + "%")
-    }else{
+    }
+    else if(cp===ip){
+        showMessage("Hey, no profit and no loss")
+    }
+    else{
         var loss = (ip - cp)*qty
         var lossPercentage = ((loss/ip)*100).toFixed(2)
         showMessage("Hey, the loss is "+ loss +" and the percent is " + lossPercentage + "%")
